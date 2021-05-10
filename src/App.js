20f00e7baf111117199c9a31936fcc0d0b164601
Sabrina Mueller <sabrina@famefox.de>
2021-05-10 21:14:37 +0200

    added react

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import Card from './Card';
+
+function App() {
+  const pokemon = [
+    { name: 'Pikachu', type: 'Elektro' },
+    { name: 'Glumanda', type: 'Feuer' },
+    { name: 'Bisasam', type: 'Pflanze' },
+    { name: 'Schiggy', type: 'Wasser' },
+    { name: 'Smettbo', type: 'Käfer' },
+    { name: 'Smettbo', type: 'Elektro' },
+    { name: 'Froggy', type: 'Frosch' },
+  ];
+
+  return (
+    <div className="App">
+      <h1>Pokemon</h1>
+      {pokemon.map((pokemon) => (
+        <Card name={pokemon.name} type={pokemon.type} />
+      ))}
+    </div>
+  );
+}
+
+export default App;
